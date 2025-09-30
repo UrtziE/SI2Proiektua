331835facb9cda457cdee9acc95359b6c6ca475e
Urtzi Etxegarai <urtzift13@gmail.com>
2025-09-30 18:53:19 +0200

    WBurtziTestOndo

--- Rides24Geltokiekin/src/test/java/testOperations/GetRidesDBWhiteTest.java
+++ Rides24Geltokiekin/src/test/java/testOperations/GetRidesDBWhiteTest.java
@@ -23,103 +23,163 @@ import exceptions.RideAlreadyExistException;
 import exceptions.RideMustBeLaterThanTodayException;
 
 public class GetRidesDBWhiteTest {
-	DataAccess db= new DataAccess();
-	TestDataAccess testdb= new TestDataAccess();
+	DataAccess db = new DataAccess();
+	TestDataAccess testdb = new TestDataAccess();
+	private EntityManager db2;
+	private String from = "Bera";
+	private String to = "Irun";
+	private String user = "tester00";
+	private String email = "tester00@gmail.com";
+	SimpleDateFormat f = new SimpleDateFormat("dd/MM/yyyy");
+	private String noiz = "25/10/2030";
+	private Date date = null;
+	private String matrikula = "9321CRNN";
+	int places = 4;
 	
-	private String from= "Bera";
-	private String to= "Irun";
-	private String user= "tester00";
-	private String email= "tester00@gmail.com";
-    SimpleDateFormat f = new SimpleDateFormat("dd/MM/yyyy");
-    private String noiz= "25/10/2030";
-    private Date date=null;
-    int places=4;
-    List<Float> prezioak= new ArrayList<Float>();
-    
-    Kotxe kotxe= new Kotxe();
-    List<String>ibilbide= new ArrayList<String>();
+	private int rideNum=-1;
+	private boolean createdCar;
+	private boolean createdDriver;
+	List<Float> prezioak = new ArrayList<Float>();
+
+	Kotxe kotxe = new Kotxe();
+	List<String> ibilbide = new ArrayList<String>();
+
+	@Before
+	public void initialize() {
+		System.out.println("Initialize	and	check	...");
+		try {
+			date = f.parse(noiz);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		prezioak = Arrays.asList(4.0f, 4.0f, 4.0f);
+		ibilbide = Arrays.asList("Bera", "Lesaka", "Irun");
+		from = "Bera";
+		to = "Irun";
+		rideNum=-1;
+		createdCar=false;
+		createdDriver=false;
+	}
+
 	@Test
 	public void testGetRides1() {
-	
+
 		try {
 			date = f.parse(noiz);
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		prezioak= Arrays.asList(4.0f,4.0f,4.0f);
-        ibilbide= Arrays.asList("Bera","Lesaka","Irun");
-       db.open();
-		List<Ride>rides= db.getRides(from, to, date);
+		prezioak = Arrays.asList(4.0f, 4.0f, 4.0f);
+		ibilbide = Arrays.asList("Bera", "Lesaka", "Irun");
+		db.open();
+		List<Ride> rides = db.getRides(from, to, date);
 		db.close();
-		List<Ride>rideExpected= new ArrayList<Ride>();
-		System.out.println(rides);
+		List<Ride> rideExpected = new ArrayList<Ride>();
 		assertEquals(rideExpected, rides);
-	
+
 	}
+
 	@Test
 	public void testGetRides2() {
-		
-		try {
-			date = f.parse(noiz);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-        prezioak= Arrays.asList(4.0f,4.0f,4.0f);
-        ibilbide= Arrays.asList("Bera","Lesaka","Irun");
-        testdb.open();
-        Driver driver=testdb.existDriver(user);
-        testdb.close();
-        if(driver==null) {
-		testdb.open();
-		driver=testdb.createDriver(user, email);
-		testdb.close();
-        }
-        
+		prezioak = Arrays.asList(4.0f, 4.0f);
+		ibilbide = Arrays.asList("Bera", "Irun");
+		to="Lesaka";
+		Driver driver = addDriver(user,email);
+		addCar(matrikula,places,driver);
+		Ride ride = addRide(from, to, date, places, prezioak, user, kotxe, ibilbide);
 		db.open();
-		db.createCar("Seat", "ibiza", "3453CRN", 4, driver);
+		List<Ride> rides = db.getRides(from, to, date);
 		db.close();
+		List<Ride> expectedRide = new ArrayList<Ride>();
+		assertEquals(expectedRide, rides);
+	}
+
+	@Test
+	public void testGetRides3() {
+		prezioak = Arrays.asList(4.0f, 4.0f);
+		ibilbide = Arrays.asList("Lesaka", "Irun");
+		Driver driver = addDriver(user,email);
+		addCar(matrikula,places,driver);
 		db.open();
-		kotxe=db.getKotxeGuztiak(driver).getFirst();
-		db.close();
-        db.open();
-		Ride ride=null;
-		try {
-			ride = db.createRide(from, to, date, places, prezioak, user, kotxe, ibilbide);
-		} catch (RideAlreadyExistException e) {
-			fail("That Ride exists");
-		} catch (RideMustBeLaterThanTodayException e) {
-			fail("Ride Must Be Later Than TOday");
-		}
-		db.close();
+		Ride ride = addRide(from, to, date, places, prezioak, user, kotxe, ibilbide);
 		db.open();
-		List<Ride>rides= db.getRides(from, to, date);
+		List<Ride> rides = db.getRides(from, to, date);
 		db.close();
-		List<Ride>expectedRide= new ArrayList<Ride>();
+		List<Ride> expectedRide = new ArrayList<Ride>();
+		assertEquals(expectedRide, rides);
+	}
+
+	@Test
+	public void testGetRides4() {
+		Driver driver = addDriver(user,email);
+		addCar(matrikula,places,driver);
+		db.open();
+		Ride ride = addRide(from, to, date, places, prezioak, user, kotxe, ibilbide);
+		db.open();
+		List<Ride> rides = db.getRides(from, to, date);
+		db.close();
+		List<Ride> expectedRide = new ArrayList<Ride>();
 		expectedRide.add(ride);
-		assertEquals(expectedRide,rides);
+		assertEquals(expectedRide, rides);
+	}
+	private boolean addCar(String matrikula, int tokiKop,Driver driver) {
+		db.open();
+		createdCar = db.createCar("Seat", "ibiza", matrikula, tokiKop, driver);
+		db.close();
+		testdb.open();
+		kotxe = testdb.getCar(matrikula);
+		testdb.close();
+		return createdCar;
+	}
+	private Driver addDriver(String user, String email) {
+		testdb.open();
+		Driver driver = testdb.existDriver(user);
+		if (driver == null) {
+			driver = testdb.createDriver(user, email);
+			createdDriver=true;
+		}else {
+			driver=null;
+			createdDriver=false;
+			fail("Gidaria jadanik existitzen da, ezin dira guztiz zehatz jakin erantzunak");
+		}
+		testdb.close();
+		return driver;
+		
+	}
+	private Ride addRide(String from, String to, Date date, int nPlaces, /* float price */ List<Float> price,
+			String driverUser, Kotxe kotxe, List<String> ibilbide) {
+		db.open();
+		Ride ride = null;
+		try {
+			ride = db.createRide(from, to, date, nPlaces, prezioak, user, kotxe, ibilbide);
+			rideNum=ride.getRideNumber();
+		} catch (RideAlreadyExistException e) {
+			fail("That Ride exists, you must change");
+		} catch (RideMustBeLaterThanTodayException e) {
+			fail("Ride Must Be Later Than Today");
+		}
+		db.close();
+		return ride;
+		
+	}
+	@After
+	public void bukatu() {
 		try {
 			testdb.open();
-			testdb.removeRide(ride.getRideNumber());
-			testdb.close();
+			if(rideNum>0) {
+			testdb.removeRide(rideNum);
 			testdb.open();
-			testdb.removeCar("3453CRN");
+			}
+			if (createdCar) {
+				testdb.removeCar(matrikula);
+			}
+			if(createdDriver) {
+				testdb.removeDriver(user);
+			}
 			testdb.close();
-			testdb.open();
-			testdb.removeDriver(user);
-			testdb.close();
-			assertTrue(true);
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			fail("Imposible");
 		}
-		
-		
-	}
-	public void testGetRides3() {
-		fail("Not yet implemented");
-	}
-	public void testGetRides4() {
-		fail("Not yet implemented");
 	}
 
 }
